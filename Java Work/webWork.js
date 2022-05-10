c22ddda259da9666fdb4c8bdca6b047e5c2c692a
Sameer2233 <94064287+Sameer2233@users.noreply.github.com>
2022-05-09 19:24:53 -0500

    Fetch

--- Java Work/webWork.js
+++ Java Work/webWork.js
@@ -1,5 +1,4 @@
 let o;
-
 function start() {
   if (typeof(o) == "undefined") {
     o = new Worker("web.js");
@@ -12,3 +11,50 @@ function stop() {
   o.terminate();
   o = undefined;
 }
+
+
+get("Sameer.text");
+async function get(file) {
+  let a = await fetch(file);
+  let b = await a.text();
+  document.getElementById("p1").innerHTML = b;
+}
+
+let myBtn =document.getElementById("myBtn");
+let content =document.getElementById("content");
+
+function getData(){
+    url="Sameer.text"
+    fetch(url).then((response)=>{
+        return response.text()
+    }).then((data)=>{
+        console.log(data)
+    })
+}
+getData();
+
+// function get(){
+//     ur="https://api.github.com/users"
+//     fetch(ur).then((response)=>{
+//         return response.json()
+//     }).then((data1)=>{
+//         // document.getElementById("p1").innerHTML=data;
+//         console.log(data1)
+//     })
+// }
+// get();
+
+// function get1(){
+//     ur1="https://dummy.restapiexample.com/api/v1/create"
+//     data='{"name":"sahmmeer9876543","salary":"123","age":"23"}'
+//     params={
+//         method: "post",
+//         headers:{
+//             "content-type":"application-JSON"
+//         },
+//         body: data
+//     }
+//     fetch(ur1,params).then(response=> response.json())
+//     .then(data=>console.log(data))
+// }
+// get1();
